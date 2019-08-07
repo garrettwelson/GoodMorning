f0bb364e67964167653d9d176cae66db47de0520
Garrett Welson <garrettwelson@gmail.com>
2019-08-07 11:40:51 -0500

    Update boilerplate names in index.js, app.json, and App.js

--- App.js
+++ App.js
@@ -2,13 +2,22 @@
 import React, { Component } from "react";
 import { Text, TextInput, View, Button, ButtonProperties } from "react-native";
 
-export default class HelloWorldApp extends Component {
+export default class GoodDay extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "",  };
+    this.state = { 
+      currentView: "main", 
+      taskData: [], 
+      weatherData: [] 
+    };
+    this.changeView = this.changeView.bind(this);
   }
 
-
+  changeView(value) {
+    this.setState({
+      currentView: value
+    })
+  }
 
   render() {
     return (
